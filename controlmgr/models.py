79a2e5d95ee09edfd73549d004f39b9e0327ddfb
Alexander Nedovizin <parkit74@gmail.com>
2020-03-14 22:12:14 +0500

    ControlManager: проводки + тест

--- controlmgr/models.py
+++ controlmgr/models.py
@@ -7,9 +7,23 @@ class ControlManager:
         :param money: сумма распределения
         """
 
-    def flow(self, row_source, row_dest):
+    def flow(self, row_source, row_dest, summa, check_summa=True):
         """Проводка движения денег между статьями
 
         :param row_source: запись-источник
         :param row_dest: запись-приёмник
+        :param summa: сумма проводки
+        :param check_summa: проверять проводку
         """
+        is_valid = True
+        if check_summa and (row_source.value + row_source.virtual_value - row_source.min_value) < summa:
+            is_valid = False
+
+        if is_valid:
+            row_source.value -= summa
+            row_source.save()
+
+            row_dest.value += summa
+            row_dest.save()
+
+        return is_valid
--- controlmgr/tests.py
+++ controlmgr/tests.py
@@ -1,3 +1,31 @@
+from datetime import date, timedelta
+
 from django.test import TestCase
 
-# Create your tests here.
+from controlmgr.models import ControlManager
+from manager.models import ManagerCashItems
+
+
+class ControlManagerTests(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(ControlManagerTests, self).__init__(*args, **kwargs)
+        self.begin_day = date.today()
+        self.end_day = self.begin_day + timedelta(days=90)
+        self.next_day = self.begin_day + timedelta(days=30)
+        self.out_day = self.begin_day + timedelta(days=91)
+
+    def test_flow(self):
+        cmgr = ControlManager()
+        mgr = ManagerCashItems(date_begin=self.begin_day, date_end=self.end_day)
+        cashitem1 = mgr.append(name_item="Testing_cmttf01", date_item=self.begin_day)
+        cashitem1.value = 10000
+        cashitem1.save()
+
+        cashitem2 = mgr.append(name_item="Testing_cmttf02", date_item=self.next_day)
+
+        result = cmgr.flow(row_source=cashitem2, row_dest=cashitem1, summa=5000)
+        self.assertFalse(result)
+
+        result = cmgr.flow(row_source=cashitem1, row_dest=cashitem2, summa=5000)
+        self.assertTrue(result)
